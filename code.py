957c65f7074a2f01beec0c5b2d56d93eb31c5bc3
shaialmog <shaike.almog@gmail.com>
2023-09-01 13:53:52 +0300

    t

--- code.py
+++ code.py
@@ -1,7 +1,14 @@
 from langchain.llms import Ollama
 from langchain.callbacks.manager import CallbackManager
 from langchain.callbacks.streaming_stdout import StreamingStdOutCallbackHandler
+from langchain.agents import load_tools
+from langchain.agents import initialize_agent
+from langchain.agents import AgentType
+
 llm = Ollama(base_url="http://localhost:11434",
              model="llama2",
              callback_manager = CallbackManager([StreamingStdOutCallbackHandler()]))
 llm("Tell me about the history of AI")
+tools = load_tools(["serpapi", "llm-math"],
+                   llm=llm,
+                   serpapi_api_key='6271283bfbd5df35b44961ef156de6717f35655db800fd50eab08152ec33f78e')
