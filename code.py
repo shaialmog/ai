f18b3b32fb95a419222258164e0f398a02ecc52e
shaialmog <shaike.almog@gmail.com>
2023-09-01 13:59:17 +0300

    t

--- code.py
+++ code.py
@@ -8,7 +8,9 @@ from langchain.agents import AgentType
 llm = Ollama(base_url="http://localhost:11434",
              model="llama2",
              callback_manager = CallbackManager([StreamingStdOutCallbackHandler()]))
-llm("Tell me about the history of AI")
 tools = load_tools(["serpapi", "llm-math"],
                    llm=llm,
                    serpapi_api_key='6271283bfbd5df35b44961ef156de6717f35655db800fd50eab08152ec33f78e')
+agent = initialize_agent(tools, llm, agent=AgentType.ZERO_SHOT_REACT_DESCRIPTION, verbose=True)
+agent.run("what is the last price of TECL stock from nasdaq?")
+
